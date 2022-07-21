fcc36584a9bdb5c81a5efffb333c2d4e89a771eb
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-21 14:16:05 +0900

    スプレッド構文 ...

--- src/index.js
+++ src/index.js
@@ -109,3 +109,37 @@ console.log(msg6);
  */
 const sayHello = (name = "guest") => console.log(`hello ${name}!`);
 sayHello("Ken");
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+const arr1 = [1, 2];
+console.log(arr1);
+console.log(...arr1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
+
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [n1, n2, ...arr3] = arr2;
+console.log(n1);
+console.log(n2);
+console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4]; //コピー
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5]; //結合
+console.log(arr7);
+
+// =でコピーを作らない。参照渡しになってしまう。
+const arr8 = arr4;
+arr8[0] = 100;
+console.log(arr4); //arr4の中身まで書き換えられてしまう
