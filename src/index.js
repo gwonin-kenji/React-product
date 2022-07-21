8033af7dcb9914a028dde06c5d8b41f10995c3c8
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-21 20:46:40 +0900

    タスクの追加機能（テキスト）

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // js上で htmlの domを作成する
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list_row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
