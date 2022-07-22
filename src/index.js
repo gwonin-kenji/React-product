22120a5e0cbd8842cf71ee76c1af19aaed83faae
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-22 13:10:45 +0900

    タスクの戻す機能

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // js上で htmlの domを作成する
   // divタグ生成
   const div = document.createElement("div");
@@ -12,7 +17,7 @@ const onClickAdd = () => {
 
   // liタグ生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -37,6 +42,15 @@ const onClickAdd = () => {
     // buttonタグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(div)を完了リストから削除する
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキストを取得する
+      const text = deleteTarget.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     // divタグの子要素に各要素を設定
     addTarget.appendChild(li);
